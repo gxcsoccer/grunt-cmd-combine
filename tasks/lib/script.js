423317ccc14ecee8dda3dea0467f49f675856d4c
gaoxiaochen <gxcsoccer@126.com>
2013-09-03 23:40:24 +0800

    update

--- tasks/lib/script.js
+++ tasks/lib/script.js
@@ -1,7 +1,11 @@
+var ast = require('cmd-util').ast;
+
 exports.init = function(grunt) {
 	var exports = {};
 
 	exports.jsConcat = function(fileObj, options) {
+		var data = grunt.file.read(fileObj.src),
+			astCache = ast.parse(data);
 
 	};
 
